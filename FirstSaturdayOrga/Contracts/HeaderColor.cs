3daab0fd0a36c1beb805815dfc4288b1f3bb9446
Sandra Setz <ssetz75@gmail.com>
2026-02-19 11:11:16 +0100

    Created component for location dashboard

--- FirstSaturdayOrga/Contracts/HeaderColor.cs
+++ FirstSaturdayOrga/Contracts/HeaderColor.cs
@@ -4,6 +4,27 @@
         Green,
         Blue,
         Yellow,
-        Orange
+        Orange,
+        Purple
+    }
+
+    public static class HeaderColorHelpers {
+        public static HeaderColor YearToColor(int year) {
+            if (year == 2023 || year == 2015)
+                return HeaderColor.Red;
+            else if (year == 2024 || year == 2016)
+                return HeaderColor.Blue;
+            else if (year == 2025 || year == 2019)
+                return HeaderColor.Green;
+            else if (year == 2026 || year == 2020)
+                return HeaderColor.Yellow;
+            else if (year == 2027)
+                return HeaderColor.Orange;
+            else if (year == 2028)
+                return HeaderColor.Purple;
+            else
+                throw new ArgumentException($"Unsupported year: {year}");
+
+        }
     }
 }
